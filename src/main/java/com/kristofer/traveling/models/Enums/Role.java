4bc2b88acba683fd6e052b8505c5ccf355049b93
kristofer krindges <thecontt06@gmail.com>
2023-07-30 12:32:59 -0300

    create, config and add userTest

---
+++ src/main/java/com/kristofer/traveling/models/Enums/Role.java
@@ -0,0 +1,6 @@
+package com.kristofer.traveling.models.Enums;
+
+public enum Role {
+ USER,
+ ADMIN
+}
---
+++ src/test/java/com/kristofer/traveling/TravelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristofer.traveling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
